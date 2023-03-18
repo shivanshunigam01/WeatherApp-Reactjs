025a1322315ea5e339ebd0259269285ccd7849c9
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-03-19 00:00:23 +0530

    #

--- src/currentLocation.js
+++ src/currentLocation.js
@@ -94,15 +94,15 @@ class Weather extends React.Component {
     clearInterval(this.timerID);
   }
 
-  // tick = () => {
-  //   this.getPosition()
-  //   .then((position) => {
-  //     this.getWeather(position.coords.latitude, position.coords.longitude)
-  //   })
-  //   .catch((err) => {
-  //     this.setState({ errorMessage: err.message });
-  //   });
-  // }
+  tick = () => {
+    this.getPosition()
+    .then((position) => {
+      this.getWeather(position.coords.latitude, position.coords.longitude)
+    })
+    .catch((err) => {
+      this.setState({ errorMessage: err.message });
+    });
+  }
 
   getPosition = (options) => {
     return new Promise(function (resolve, reject) {
