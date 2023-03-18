3d1bedb7df6b277bd3cefeaaf2eaf33951af3223
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-03-18 22:58:38 +0530

    18-03-2023

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "{8a1bcb85373a73082ccbf416b6b91dd6}",
   base: "https://api.openweathermap.org/data/2.5/",
 };
